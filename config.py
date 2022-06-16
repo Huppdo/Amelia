86f14b3acea8b2929ab3e5725b9943dac1cdfe91
Dominic Hupp <dhupp2020@gmail.com>
2022-06-16 09:59:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 botName = "Amelia"
 userName = "Dom"
-currentZip = "48390"
+currentZip = "43015"
 minConfidence = 0.8
 highConfidence = 0.97
 textEntry = False
 textOutput = False
 
 #twitter stuffs
-twitterAPIKey = "EYNSqxi0ZTTw3FtWW7KdWXXBN"
-twitterAPISecret = "FYUGSk9680rfomGF01rRVAlL2hwwCL4OXvYiX5q9WO5KhmqGhz"
-twitterAccessToken = "766297985156259840-khU3pXJu6Cx9MdVflTEqieEWFOwiLcA"
-twitterAccessSecret = "Ctm0ajVpGYCPOy11A51KtEGwIGyxAItd8Qo3q6dNmco63"
+twitterAPIKey = ""
+twitterAPISecret = ""
+twitterAccessToken = ""
+twitterAccessSecret = ""
 
 #spotify stuffs
-spotifyPersonalAuth = "fuAVmj7usVGCwYmPqewdmjh56Ry4mMa2HGc73WMB4f4mUw"
+spotifyPersonalAuth = ""
